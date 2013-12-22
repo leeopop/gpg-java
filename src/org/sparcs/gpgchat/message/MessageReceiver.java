7c1bd2197772b3d3a017cbaa741d2ac78b448616
leeopop <dlrmsghd@gmail.com>
2013-12-22 16:34:25 +0900

    Messaging interface

---
+++ src/org/sparcs/gpgchat/message/MessageReceiver.java
@@ -0,0 +1,6 @@
+package org.sparcs.gpgchat.message;
+
+public interface MessageReceiver {
+
+	public void receiveMessage(String message);
+}
---
+++ src/org/sparcs/gpgchat/message/MessageSender.java
@@ -0,0 +1,6 @@
+package org.sparcs.gpgchat.message;
+
+public interface MessageSender {
+
+	public void sendMessage(String message);
+}
